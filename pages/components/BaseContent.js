77f43da4188e7a14edbf1e9d52c1bede7b4c1939
Aoshi Fujioka <blue20will@gmail.com>
2020-10-08 01:28:00 +0900

    :zap: Move definition of style .content

--- pages/components/BaseContent.js
+++ pages/components/BaseContent.js
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types';
 
-import styles from './BaseContent.module.scss';
-
 export default function BaseContent(props) {
   return (
     <div className="container">
-      <div className={styles.content}>
+      <div className="content">
         <h3>{props.heading}</h3>
         <p>{props.children}</p>
       </div>
